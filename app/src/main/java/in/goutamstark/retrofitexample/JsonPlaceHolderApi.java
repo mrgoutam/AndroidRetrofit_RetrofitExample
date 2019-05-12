2d5a20b0e2ac2908b7378805d150f23c35f0bbe7
mrgoutam <mrgoutam8.3@gmail.com>
2019-05-12 20:19:09 +0530

    All Path, Query, QueryMap and Url completed

--- app/src/main/java/in/goutamstark/retrofitexample/JsonPlaceHolderApi.java
+++ app/src/main/java/in/goutamstark/retrofitexample/JsonPlaceHolderApi.java
@@ -1,12 +1,41 @@
 package in.goutamstark.retrofitexample;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
     Call<List<Post>> getPosts();
+
+    @GET("posts/{id}/comments")
+    Call<List<Comments>> getComments(@Path("id") int postId);
+
+
+    //http://jsonplaceholder.typicode.com/posts?userId=2
+    @GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") int userId);
+
+
+    //http://jsonplaceholder.typicode.com/posts?userId=2&_sort=id&_order=desc
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") int userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order);
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET
+    Call<List<Comments>> getComments(@Url String url);
+
+
 }
--- app/src/main/java/in/goutamstark/retrofitexample/MainActivity.java
+++ app/src/main/java/in/goutamstark/retrofitexample/MainActivity.java
@@ -6,7 +6,9 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,44 +26,217 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewResult = findViewById(R.id.text_view_result);
-        try {
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("http://jsonplaceholder.typicode.com/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-            JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        //getPosts();
+        //getComments();
+        //getPostsByPostData();
+        //getPostsByMultiplePostData();
+        //getPostsByMap();
+        getCommentsByUrl();
+    }
 
-            Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
-            call.enqueue(new Callback<List<Post>>() {
-                @Override
-                public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                    if (!response.isSuccessful()) {
-                        textViewResult.setText("Code: " + response.code());
-                        return;
-                    }
+    private void getPosts() {
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
 
-                    List<Post> posts = response.body();
-
-                    String data = "";
-                    for (Post post : posts) {
-                        data += "ID: " + post.getId() + "\n";
-                        data += "ID: " + post.getUserId() + "\n";
-                        data += "ID: " + post.getTitle() + "\n";
-                        data += "ID: " + post.getText() + "\n\n";
-                    }
-
-                    textViewResult.append(data);
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
                 }
 
-                @Override
-                public void onFailure(Call<List<Post>> call, Throwable t) {
-                    textViewResult.setText(t.getMessage());
+                List<Post> posts = response.body();
+
+                String data = "";
+                for (Post post : posts) {
+                    data += "ID: " + post.getId() + "\n";
+                    data += "ID: " + post.getUserId() + "\n";
+                    data += "ID: " + post.getTitle() + "\n";
+                    data += "ID: " + post.getText() + "\n\n";
                 }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    //https://www.youtube.com/watch?v=TyJEDhauUeQ&list=PLrnPJCHvNZuCPNCW2xdriIUgxmo9-QcFi&index=2
+    private void getComments() {
+        Call<List<Comments>> call = jsonPlaceHolderApi.getComments(3);
+
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comments> comments = response.body();
+
+                String data = "";
+                for (Comments comment : comments) {
+                    data += "ID: " + comment.getId() + "\n";
+                    data += "Name: " + comment.getName() + "\n";
+                    data += "Email: " + comment.getEmail() + "\n";
+                    data += "Post ID: " + comment.getPostId() + "\n";
+                    data += "Body: " + comment.getText() + "\n\n";
+                }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
+    }
+
+    //https://www.youtube.com/watch?v=TyJEDhauUeQ&list=PLrnPJCHvNZuCPNCW2xdriIUgxmo9-QcFi&index=2
+    private void getPostsByPostData() {
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(2);
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                String data = "";
+                for (Post post : posts) {
+                    data += "ID: " + post.getId() + "\n";
+                    data += "ID: " + post.getUserId() + "\n";
+                    data += "ID: " + post.getTitle() + "\n";
+                    data += "ID: " + post.getText() + "\n\n";
+                }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    //https://www.youtube.com/watch?v=TyJEDhauUeQ&list=PLrnPJCHvNZuCPNCW2xdriIUgxmo9-QcFi&index=2
+    private void getPostsByMultiplePostData() {
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(2,"id","desc");
+        //Call<List<Post>> call = jsonPlaceHolderApi.getPosts(2,null,null);
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                String data = "";
+                for (Post post : posts) {
+                    data += "ID: " + post.getId() + "\n";
+                    data += "ID: " + post.getUserId() + "\n";
+                    data += "ID: " + post.getTitle() + "\n";
+                    data += "ID: " + post.getText() + "\n\n";
+                }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getPostsByMap() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId","2");
+        parameters.put("_sort","id");
+        parameters.put("_order","desc");
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                String data = "";
+                for (Post post : posts) {
+                    data += "ID: " + post.getId() + "\n";
+                    data += "ID: " + post.getUserId() + "\n";
+                    data += "ID: " + post.getTitle() + "\n";
+                    data += "ID: " + post.getText() + "\n\n";
+                }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getCommentsByUrl() {
+        Call<List<Comments>> call = jsonPlaceHolderApi.getComments("posts/3/comments");
+
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comments> comments = response.body();
+
+                String data = "";
+                for (Comments comment : comments) {
+                    data += "ID: " + comment.getId() + "\n";
+                    data += "Name: " + comment.getName() + "\n";
+                    data += "Email: " + comment.getEmail() + "\n";
+                    data += "Post ID: " + comment.getPostId() + "\n";
+                    data += "Body: " + comment.getText() + "\n\n";
+                }
+
+                textViewResult.append(data);
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
     }
 }
